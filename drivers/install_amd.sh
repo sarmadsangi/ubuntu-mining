ea11e8b2bca8b9b51086bf32fdb6c8ba0d5dc3d9
Sarmad Sangi <sarmadsangi@gmail.com>
2018-01-08 13:14:17 +0800

    Update install_amd.sh

--- drivers/install_amd.sh
+++ drivers/install_amd.sh
@@ -9,6 +9,8 @@ cd amdgpu-pro-17.40-492261
 ./amdgpu-pro-install –y
 
 # Configure and reboot
+# Also make sure you add/update GRUB_CMDLINE_LINUX_DEFAULT="quiet splash amdgpu.vm_fragment_size=9"
+# to /etc/default/grub and update grup by sudo update-grub
 echo 'export LLVM_BIN=/opt/amdgpu-pro/bin' | sudo tee /etc/profile.d/amdgpu-pro.sh
 sudo reboot 
 
